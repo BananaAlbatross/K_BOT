7a172f5e4a5789b7c4a8db55c582e1fa2d1cb680
BananaAlbatross <kaarencorneliussinijarv@gmail.com>
2025-12-28 18:42:24 +0200

    Correct depth-1 evaluating bot

--- main.py
+++ main.py
@@ -5,29 +5,42 @@ import re
 play = True
 
 board = chess.Board()
-
+board.pieces
 def evaluate(board):
     score = 0
-    scores = {  "p": -100,  "b": -300,  "n": -300,  "r": -500,  "q": -900,  "k": -30000, 
-                "P": 100,   "B": 300,   "N": 300,   "R": 500,   "Q": 900,   "K": 30000,
-                "1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8": 0}
-    for i in re.sub(r'\d+', '', board.fen().split()[0].replace("/", "")):
-        score += scores[i]
+    scores = {
+        chess.PAWN: 100,
+        chess.BISHOP: 300,
+        chess.KNIGHT: 300,
+        chess.ROOK: 500,
+        chess.QUEEN: 900,
+        chess.KING: 30000
+    }
+    for i in scores:
+        # Loendame nuppe ja arvutame skoori
+        score += scores[i] * len(board.pieces(i, True)) # True on valge
+        score -= scores[i] * len(board.pieces(i, False)) # False on must
     return score
 
 if play:
     while not board.is_game_over():
+        turn = board.turn
         best_move = None
-        highest_value = -63000
+        highest_value = -63000 if turn else 63000
 
         legal_moves = list(board.legal_moves)
         for i in legal_moves:
             board.push(i)
             value = evaluate(board)
             board.pop()
-            if value > highest_value:
-                highest_value = value
-                best_move = i
+            if turn:
+                if value > highest_value:
+                    highest_value = value
+                    best_move = i
+            else:
+                if value < highest_value:
+                    highest_value = value
+                    best_move = i
                 
         board.push(best_move)
         print(board)
